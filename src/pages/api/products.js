504a0284050db7dd21043397614c46654564b3a6
Your Full Name <username@users.noreply.github.com>
2025-01-04 02:15:10 +0500

    new update

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -2,39 +2,39 @@ export default function handler(req, res) {
     const products = [
         {
             id: 1,
-            name: "Spicy Pepperoni Pizza",
+            name: "BBQ Chicken Pizza",
             price: 149, // Corrected syntax
-            image: "https://i.pinimg.com/736x/3f/d6/e8/3fd6e8dac810890aab944aa77b380665.jpg", // Changed to 'image'
+            image: "/images/BBQ Chicken Pizza.png", // Changed to 'image'
         },
         {
             id: 2,
-            name: "Cheesy Garlic Pizza",
+            name: "Cheese Lover Pizza",
             price: 149,
-            image: "https://i.pinimg.com/236x/f2/99/96/f29996790936260eb1879226910d09b3.jpg",
+            image: "/images/Cheese Lover Pizza.png",
         },
         {
             id: 3,
-            name: "Classic Margherita Pizza",
+            name: "Margherita Pizza",
             price: 149,
-            image: "https://i.pinimg.com/236x/0c/b0/57/0cb05762415e64d86d01975b7bf510b2.jpg",
+            image: "/images/Margherita Pizza.png",
         },
         {
             id: 4,
-            name: "Veggie Supreme Pizza",
+            name: "Sicilian Pizza",
             price: 149,
-            image: "https://i.pinimg.com/236x/7f/15/d1/7f15d1ca5907819e607f2166445e87a2.jpg",
+            image: "/images/Sicilian Pizza.png",
         },
         {
             id: 5,
-            name: "BBQ Chicken Pizza",
+            name: "Pepperoni Pizza",
             price: 149,
-            image: "https://i.pinimg.com/236x/7a/f1/da/7af1daa3b7bf442bd3200fd7e3eaa2b4.jpg",
+            image: "/images/Pepperoni Pizza.png",
         },
         {
             id: 6,
-            name: "Meat Lover's Pizza",
+            name: "Veggie Pizza",
             price: 149,
-            image: "https://i.pinimg.com/236x/3d/de/49/3dde496586cf51a2d6f6fb87bdab76db.jpg",
+            image: "/images/Veggie Pizza.png",
         },
     ];
     res.status(200).json(products);
